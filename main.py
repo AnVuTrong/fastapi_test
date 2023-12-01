d3d718ab38db6e67a6f97708f264b932b8847745
anvutrong <vta.bsn@gmail.com>
2023-12-01 18:27:11 +0700

    more function

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'data': 'blog list'}
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -17,6 +28,17 @@ def show(id: int):
     return {'data': id}
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    # create blog
+    return {'data': 'Blog is created with title {blog.title}'}
